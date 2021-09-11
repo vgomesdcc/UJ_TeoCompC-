bf768a01d0aad33b49d1668017f56ceac1bef145
vgomesdcc <40807939+vgomesdcc@users.noreply.github.com>
2021-09-11 14:08:59 -0300

    Update p4.cs

--- p4.cs
+++ p4.cs
@@ -1,22 +1,30 @@
 using System;
 
-class Program
-{
-  static void Main(string[] args)
-  {
-		
-    Console.WriteLine("Digite o primeiro numero:");
-		float numerador = float.Parse(Console.ReadLine());
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Insira os numeros:");
+    int perfeito1 = int.Parse(Console.ReadLine());
+    int perfeito2 = int.Parse(Console.ReadLine());
 
-    Console.WriteLine("Digite o segundo numero:");
-		float denominador = float.Parse(Console.ReadLine());
+    int flag1=0;
+    int flag2=0;
 
-		if(numerador%denominador==0){
-		  Console.WriteLine("Divisores perfeitos");
+    for(int i=1; i<perfeito1; i++){
+		  if(perfeito1%i==0){
+			  flag1+=i;
+		  }
 	  }
 
-    else
-      Console.WriteLine("Nao sao divisores perfeitos");
+    for(int z=1; z<perfeito2; z++){
+		  if(perfeito2%z==0){
+		  	flag2+=z;
+		  }
+	  }
 
+    if(perfeito1==flag1 && perfeito2==flag2){
+		 Console.WriteLine("Sao divisores perfeitos\n");
+	}
+	else
+		 Console.WriteLine("Nao sao divisores perfeitos\n");
   }
 }
